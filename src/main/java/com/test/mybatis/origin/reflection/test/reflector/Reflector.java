b9f33a88f6928aee5238f9817edf25a6c646f4fb
王玉东 <1174872613@qq.com>
2023-01-06 09:53:24 +0800

    调整目录结构

--- src/main/java/com/test/mybatis/origin/reflection/test/reflector/Reflector.java
+++ src/main/java/com/test/mybatis/origin/reflection/test/reflector/Reflector.java
@@ -1,19 +1,17 @@
-package com.test.mybatis.origin.reflection;
+package com.test.mybatis.origin.reflection.test.reflector;
 
 import com.test.mybatis.use.pojo.UserDO;
 import org.apache.ibatis.reflection.DefaultReflectorFactory;
-import org.apache.ibatis.reflection.Reflector;
 import org.apache.ibatis.reflection.ReflectorFactory;
 import org.apache.ibatis.reflection.TypeParameterResolver;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 
-public class TestReflector {
+public class Reflector {
 
     public static void main(String[] args) throws Exception {
-        Reflector reflector = new Reflector(UserDO.class);
+        org.apache.ibatis.reflection.Reflector reflector = new org.apache.ibatis.reflection.Reflector(UserDO.class);
         ReflectorFactory factory = new DefaultReflectorFactory();
 
         //测试TypeParameterResolver
