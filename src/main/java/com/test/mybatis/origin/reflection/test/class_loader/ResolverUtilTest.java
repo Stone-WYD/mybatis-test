a6e431b92d54f37f326c228037bcb4ecdf11d390
Stone-WYD <1174872613@qq.com>
2024-09-05 19:09:31 +0800

    Resolver 包内容查找

---
+++ src/main/java/com/test/mybatis/origin/reflection/test/class_loader/ResolverUtilTest.java
@@ -0,0 +1,22 @@
+package com.test.mybatis.origin.reflection.test.class_loader;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.binding.MapperRegistry;
+import org.apache.ibatis.io.ResolverUtil;
+
+/**
+ * @author xh
+ * @date 2024-09-02
+ * @Description:
+ */
+public class ResolverUtilTest {
+
+    public static void main(String[] args) {
+        ResolverUtil<Object> resolverUtil = new ResolverUtil<>();
+        ResolverUtil<Object> annotatedResolverUtil = resolverUtil.findAnnotated(Mapper.class, "com.test.mybatis.use.proxy");
+        for (Class<?> aClass : annotatedResolverUtil.getClasses()) {
+            System.out.println(aClass);
+        }
+
+    }
+}
--- src/main/java/com/test/mybatis/use/proxy/UserMapper.java
+++ src/main/java/com/test/mybatis/use/proxy/UserMapper.java
@@ -1,7 +1,11 @@
 package com.test.mybatis.use.proxy;
 
 import com.test.mybatis.use.pojo.UserDO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+@Mapper
 public interface UserMapper {
     UserDO findUserById(int id);
+
 }
